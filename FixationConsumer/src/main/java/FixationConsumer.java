97503c991da91193a628ff0cee744c010ffa7a89
Amine Abbad-Andaloussi <amine.abbad-andaloussi@unisg.ch>
2024-05-01 13:40:45 +0200

    Avro relatedd updates

--- FixationConsumer/src/main/java/FixationConsumer.java
+++ FixationConsumer/src/main/java/FixationConsumer.java
@@ -1,5 +1,6 @@
 import com.google.common.io.Resources;
-import magicalpipelines.model.TranslatedFixation;
+
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -16,7 +17,7 @@ public class FixationConsumer {
     public static void main(String[] args) throws IOException {
 
             // Read Kafka properties file and create Kafka consumer with the given properties
-            KafkaConsumer<String, TranslatedFixation> consumer;
+            KafkaConsumer<String, GenericRecord> consumer;
             try (InputStream props = Resources.getResource("consumer.properties").openStream()) {
                 Properties properties = new Properties();
                 properties.load(props);
@@ -30,12 +31,12 @@ public class FixationConsumer {
             while (true) {
 
                 // pool new data
-                ConsumerRecords<String, TranslatedFixation> records = consumer.poll(Duration.ofMillis(8));
+                ConsumerRecords<String, GenericRecord> records = consumer.poll(Duration.ofMillis(8));
 
                 // process consumer records
-                for (ConsumerRecord<String, TranslatedFixation> record : records) {
+                for (ConsumerRecord<String, GenericRecord> record : records) {
 
-                            String value = record.value().toString();
+                            GenericRecord value = record.value();
                             String cl = record.partition()==1? "High": "Low";
                             System.out.println("Received fixation event: " + value + "- Cognitive load: " + cl);
 
